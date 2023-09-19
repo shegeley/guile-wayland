d18d949c16439fe4d6635ba94246d274de638132
Grigory Shepelev <shegeley@gmail.com>
2023-09-19 08:44:49 +0300

    identation

--- modules/wayland/client/protocol/wayland.scm
+++ modules/wayland/client/protocol/wayland.scm
@@ -2,4 +2,5 @@
   #:use-module (bytestructure-class)
   #:use-module (wayland scanner)
   #:use-module (wayland config))
+
 (use-wayland-protocol (%wayland-protocol #:type client))
--- modules/wayland/server/protocol/wayland.scm
+++ modules/wayland/server/protocol/wayland.scm
@@ -1,5 +1,7 @@
 (define-module (wayland server protocol wayland)
+
   #:use-module (bytestructure-class)
   #:use-module (wayland scanner)
   #:use-module (wayland config))
+
 (use-wayland-protocol (%wayland-protocol #:type server))
