65c77d755bb991e19231a82fc12a18a2f156e4bf
Grigory Shepelev <shegeley@gmail.com>
2023-09-19 11:19:58 +0300

    + wayland/client/protocol/text-input

---
+++ modules/wayland/client/protocol/text-input.scm
@@ -0,0 +1,10 @@
+(define-module (wayland client protocol text-input)
+  #:use-module (bytestructure-class)
+  #:use-module (wayland scanner)
+  #:use-module (wayland config))
+
+(eval-when (compile)
+  (define text-input.xml
+    (string-append %wayland-protocols-dir "/wayland-protocols/unstable/text-input/text-input-unstable-v3.xml")))
+
+(use-wayland-protocol (text-input.xml #:type client))
