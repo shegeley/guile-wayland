9936106b474b9670cad5a9f1b9a4307215da9bb0
Grigory Shepelev <shegeley@gmail.com>
2023-09-24 08:43:33 +0300

    + wayland/client/protocol/input-method

---
+++ modules/wayland/client/protocol/input-method.scm
@@ -0,0 +1,11 @@
+(define-module (wayland client protocol input-mtthod)
+  #:use-module (wayland client protocol wayland)
+  #:use-module (bytestructure-class)
+  #:use-module (wayland scanner)
+  #:use-module (wayland config))
+
+(eval-when (compile)
+  (define input-method.xml
+    (string-append %wayland-protocols-dir "/wayland-protocols/unstable/input-method/input-method-unstable-v1.xml")))
+
+(use-wayland-protocol (input-method.xml #:type client))
