a0b37aaef3f0fedaa17b1e2918080ed54d43b418
Grigory Shepelev <shegeley@gmail.com>
2023-10-06 07:20:32 +0300

    + tests/scanner.scm

--- tests/scanner.scm
+++ tests/scanner.scm
@@ -4,7 +4,8 @@
   #:use-module (wayland config)
   ;; NOTE: maybe including whole guix as dependency is a bad idea
   ;; should be a way to move (guix build utils) "outside"
-  #:use-module (guix build utils))
+  #:use-module (guix build utils)
+  #:use-module (srfi srfi-64))
 
 (define (xml? filename)
   (string-suffix-ci? ".xml" filename))
@@ -17,8 +18,14 @@
 
 (map
  (lambda (p)
-   (use-wayland-protocol p #:type 'client)
-   (use-wayland-protocol p #:type 'server))
+   (test-assert (string-append (basename p)
+                               ":"
+                               "server")
+     (use-wayland-protocol (p #:type client)))
+   (test-assert (string-append (basename p)
+                               ":"
+                               "client")
+     (use-wayland-protocol (p #:type server))))
  protocols)
 
 (test-end)
